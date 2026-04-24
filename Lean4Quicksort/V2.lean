b7f5b1f040761a3ef23de70b126cf737abd74686
Johan Henkel <johan.p.henkel@gmail.com>
2026-04-24 17:37:50 +0200

    Session changes 3

--- Lean4Quicksort/V2.lean
+++ Lean4Quicksort/V2.lean
@@ -39,6 +39,7 @@ def dnfhelper2 [Ord α] [ToString α]
     match compare (arr[unproc]) pvt with
     | .lt =>
       if hfin : unproc ≥ fin_unproc then ⟨((arr.swap unproc eq (by omega) (by omega)), eq + 1, fin_unproc + 1), (by simp; omega)⟩ else
+      if compare arr[eq] arr[unproc] = .lt then dnfhelper2 arr pvt (eq + 1) (unproc + 1) fin_unproc sllo slhi (by omega) (by omega) (by omega) (by omega) (by omega) (by omega) else
       dnfhelper2 (arr.swap unproc eq (by omega) (by omega)) pvt (eq + 1) (unproc + 1) fin_unproc sllo slhi (by omega) (by omega) (by omega) (by omega) (by omega) (by omega)
 
     | .gt =>
@@ -96,4 +97,4 @@ def Vector.quicksort2 [Ord α] [ToString α] {size} (arr : Vector α size) : Vec
 /- testing -/
 
 def demoArray2 : Array Nat := #[47, 13, 82, 6, 91, 34, 57, 23, 76, 41, 88, 3, 65, 29, 54, 17, 72, 39, 84, 11, 63, 28, 95, 42, 7, 56, 31, 78, 19, 67, 44, 90, 25, 58, 14, 83, 37, 62, 9, 71, 48, 26, 93, 15, 52, 38, 77, 22, 69, 4, 86, 33, 61, 18, 45, 79, 12, 57, 35, 81, 24, 68, 43, 96, 8, 53, 27, 74, 16, 89, 41, 64, 30, 55, 20, 73, 46, 85, 10, 60, 36, 92, 21, 49, 66, 32, 75, 5, 87, 40, 59, 28, 70, 38, 94, 50, 80, 2, 97, 44]
-#eval! (Array.quicksort2 (Array.range 32)) == demoArray2.qsort -- is sorted:
+#eval! (Array.quicksort2 demoArray2) == demoArray2.qsort -- is sorted:
